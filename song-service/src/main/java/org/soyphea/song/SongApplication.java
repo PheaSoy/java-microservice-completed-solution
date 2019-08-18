8197b85f204afb0d0a08928a543d53ca1a98177e
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2019-08-18 15:09:35 +0700

    Update song-service cofig

--- song-service/src/main/java/org/soyphea/song/SongApplication.java
+++ song-service/src/main/java/org/soyphea/song/SongApplication.java
@@ -1,5 +1,7 @@
 package org.soyphea.song;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,9 +9,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 
-public class SongApplication {
+public class SongApplication  implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SongApplication.class,args);
+    }
+
+    @Value("${favorite.song}")
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
