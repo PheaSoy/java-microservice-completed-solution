c38a0f163539a8d612ed405e42ba1413e0bee096
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2019-08-18 14:15:13 +0700

    Upate song-service with expose specific endpoint only.

--- configure-server/src/main/java/org/soyphea/config/ConfigureServerApplication.java
+++ configure-server/src/main/java/org/soyphea/config/ConfigureServerApplication.java
@@ -1,31 +1,14 @@
 package org.soyphea.config;
 
-import java.util.Optional;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigureServerApplication {
-	
-	public static void main(String[] args) {
-		//SpringApplication.run(ConfigureServerApplication.class, args);
-		new ConfigureServerApplication().test1();
-	}
-	
-	
-	public  void test1() {
-		
-		String text = "pl";
-		//String defaultText = Optional.ofNullable(text).orElseGet(this::getDefaultValue);
-		String defaultText = Optional.ofNullable(text).orElse(getDefaultValue());
-		System.out.println(defaultText);
-		
-	}
-	public String getDefaultValue() {
-	    System.out.println("Getting Default Value11");
-	    return "Default Value";
-	}
+
+    public static void main(String args[]){
+        SpringApplication.run(ConfigureServerApplication.class,args);
+    }
 }
