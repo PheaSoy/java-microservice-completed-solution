ef95c27920b314c4749543551ef456c9dc575500
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2019-08-18 15:42:39 +0700

    Disable servlet context path for configure-server.

--- song-service/src/main/java/org/soyphea/song/SongApplication.java
+++ song-service/src/main/java/org/soyphea/song/SongApplication.java
@@ -1,5 +1,7 @@
 package org.soyphea.song;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,13 +13,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 public class SongApplication  implements CommandLineRunner {
 
+    Logger logger = LoggerFactory.getLogger(SongApplication.class);
+
+
     public static void main(String[] args) {
         SpringApplication.run(SongApplication.class,args);
     }
 
-    @Value("${favorite.song}")
+    @Value("${favorite.song:default-song}")
+    String favoriteSong;
+
     @Override
     public void run(String... args) throws Exception {
-
+        logger.info("Favorite song fetched from configure-server:{}",favoriteSong);
     }
 }
